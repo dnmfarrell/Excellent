73bc4a955c00dd9af7a14fcb98aaa947564a2c84
David Farrell <davidnmfarrell@gmail.com>
2015-10-16 23:12:58 -0400

    use integer division instead of /

--- Excellent.hs
+++ Excellent.hs
@@ -2,7 +2,7 @@ module Excellent where
 
 intLen :: Int -> Int
 intLen x = ceiling $ logBase 10 $ fromIntegral y
-  where y = if x `mod` 10 == 0 then x + 1 else x
+  where y = if mod x 10 == 0 then x + 1 else x
 
 intSplit :: Int -> Maybe (Int, Int, Int)
 intSplit x
@@ -10,10 +10,19 @@ intSplit x
   | odd l     = Nothing
   | otherwise = Just (x, top, bottom)
   where l      = intLen x
-        h      = ceiling (fromIntegral l / 2)
-        bottom = x `mod` (10^h)
-        top    = (x - bottom) `div` (10^h)
+        h      = div l 2
+        bottom = mod x (10^h)
+        top    = div (x - bottom) (10^h)
 
 intExcellent :: Maybe (Int,Int,Int) -> Bool
 intExcellent Nothing        = False
 intExcellent (Just (x,y,z)) = x == ((z*z) - (y*y))
+
+intRectangular :: Int -> Bool
+intRectangular x = x == y * y - y
+  where y = ceiling $ sqrt $ fromIntegral x
+
+intExcellentTopHalf :: Int -> Bool
+intExcellentTopHalf x = intRectangular (x * p + x)
+  where k = intLen x
+        p = 10^k
